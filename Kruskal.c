3303d8e4e5e8212fe2a6e07b41b5ba24abcffd4e
Juliano-hub <juliano.buss@gmail.com>
2021-05-02 16:44:44 -0300

    initial upload

--- Kruskal.c
+++ Kruskal.c
@@ -10,7 +10,6 @@ typedef struct{
 Aresta* Criar(int Total_arestas);
 void PreencheStruct(Aresta* Grafo);
 void Ordernar(Aresta* Grafo, int Total_Arestas);
-void ZeraAux(Aresta* MST, int Total_Vertices);
 void Kruskal(Aresta* Grafo, Aresta* MST, int *Conjunto, int Total_Arestas, int Total_Vertices);
 void PreencheConjunto(int *Conjunto, int Total_Vertices);
 int Verifica_Conjunto(int origem, int destino, int *Conjunto, int Total_Vertices);
@@ -27,48 +26,48 @@ int main (){
     // cria as arestas e preenche os valores
     Grafo = Criar(Total_Arestas);
     PreencheStruct(Grafo);
-
-    for(int i=0 ; i<Total_Arestas; i++){
-        printf("\nOrigem:%d ", (Grafo+i)->origem);
-        printf("Destino:%d ", (Grafo+i)->Destino);
-        printf("Valor:%d ", (Grafo+i)->valor);
-    }
-
     // aplica o algoritmo bubbleshort para fazer a ordenação das arestas pelo seu valor/peso
     Ordernar(Grafo, Total_Arestas);
 
-    printf("\n-----Apos ordernar:\n");
-    for(int i=0 ; i<Total_Arestas; i++){
-        printf("\nOrigem:%d ", (Grafo+i)->origem);
-        printf("Destino:%d ", (Grafo+i)->Destino);
-        printf("Valor:%d ", (Grafo+i)->valor);
-    }
+    /*
+        printf("\n-----Apos ordernar:\n");
+        for(int i=0 ; i<Total_Arestas; i++){
+            printf("\nOrigem:%d ", (Grafo+i)->origem);
+            printf("Destino:%d ", (Grafo+i)->Destino);
+            printf("Valor:%d ", (Grafo+i)->valor);
+        }
+    */
 
     // cria um auxiliar para receber as arestas válidas
     MST = Criar( (Total_Vertices - 1) );
-    // zera todos os valores dela para usar na comparação depois dentro do algoritmo em si
-    ZeraAux(MST, (Total_Vertices - 1) );
 
-    // uso o 1 para separar os valores, assim eles estão em "conjuntos" diferentes, ex: 131141, o valor 3 e 4 estão em conj diferentes
-    Conjunto = (int*) malloc( (Total_Vertices * 3) * sizeof(int) );
-    PreencheConjunto(Conjunto, (Total_Vertices * 3) );
+    Conjunto = (int*) malloc( (Total_Vertices) * sizeof(int) );
+
+    // preenche o conjunto com o seu próprio indice, para fazer a agrupação dos conjuntos depois
+    PreencheConjunto(Conjunto, (Total_Vertices) );
 
     // usa o algoritmo de Kruskal
     Kruskal(Grafo, MST, Conjunto, Total_Arestas, Total_Vertices);
 
-
-    printf("\n No fim:\n");
-    for(int i=0 ; i<Total_Vertices*3; i = i + 3){
-        printf("%d%d%d ", Conjunto[i], Conjunto[i+1], Conjunto[i+2]);
-    }
+    /*
+        printf("\n No fim:\n");
+        for(int i=0 ; i<Total_Vertices; i++){
+            printf("%d ", Conjunto[i]);
+        }
+    */
 
     printar_resultado(MST, Total_Vertices);
+
+    free(Grafo);
+    free(MST);
 
     return 0;
 }
 
 void printar_resultado(Aresta *MST, int Total_Vertices){
     int i;
+
+    printf("Professor, coloquei para o programa iniciar com mesmo grafo do ultimo video\n");
     printf("\na = 0, b = 1, c = 2, d = 3, e = 4, f = 5, g = 6, h = 7, i = 8");
     printf("\n-----As arestas resultantes sao:\n");
     for(i=0; i< Total_Vertices - 1; i++){
@@ -76,106 +75,58 @@ void printar_resultado(Aresta *MST, int Total_Vertices){
         printf("Destino:%d ", (MST+i)->Destino);
         printf("Valor:%d ", (MST+i)->valor);        
     }
-
+    printf("\n");
 }
 
 void PreencheConjunto(int *Conjunto, int Total_Vertices){
-    int i, j=0;
-    for(i=0 ; i<Total_Vertices; i = i + 3, j++){
-        Conjunto[i] = -1;
-        Conjunto[i + 1] = j;
-        Conjunto[i + 2] = -1;
-    }
-
-        printf("\n");
-    for(i=0 ; i<Total_Vertices; i = i + 3){
-        printf("%d%d%d ", Conjunto[i], Conjunto[i+1], Conjunto[i+2]);
+    int i;
+    for(i=0 ; i<Total_Vertices; i++){
+        // preenche o conjunto com o seu próprio indice, para fazer a agrupação dos conjuntos depois
+        Conjunto[i] = i;
     }
 }
 
 int Verifica_Conjunto(int origem, int destino, int *Conjunto, int Total_Vertices){
-    int i=0, j;
-
-    while(Conjunto[i] != origem){
-        i++;
+    if(Conjunto[origem] != Conjunto[destino]){
+        return 1;
+    }else{
+        return 10;
     }
-    // cheguei no valor da origem da aresta
-
-    j = i;
-
-    if(Conjunto[i - 1] != -1){
-    //se estiver em uma posição que não é a primeira do conjunto
-
-        while(Conjunto[i] != -1){
-            //----------eu vou voltar até achar o inicio desse conjunto
-            if(Conjunto[i] == destino){
-                // se o valor do destino estiver em uma posição atrás do inicio nesse 
-                //conjunto, eu posso sair dessa função, já q eles estão juntos
-                return 10;
-            }
-        i = i - 1;    
-        }
-    }
-
-    while(Conjunto[j] != -1){
-    //----------se eu procurei pelas posições atrás e não encontrei o destino, vou procurar até eu achar o outro 1,
-    //significando que o destino pode estar a uma posição a frente do inicio
-
-        if(Conjunto[j] == destino){
-            // se o valor do destino estiver em uma posição na frente do inicio nesse 
-            //conjunto, eu posso sair dessa função, já q eles estão juntos
-            return 10;
-        }
-        j++;       
-    }
-
-    //----------só vai cair aqui caso eles não estiverem no mesmo conjunto
-    i=0;
-    while(Conjunto[i] != destino){
-        // procuro pela posição do destino
-        i++;
-    }
-
-    // como antes eu já salvei em j a posição do extremo 1, já recupero ele para fazer a junção dos conjuntos
-    while(Conjunto[i] != -1){
-            Conjunto[j] = Conjunto[i];
-            Conjunto[i] = -1;
-        i++;
-        j++;
-    }
-
-    printf("\n No meio:\n");
-    for(int i=0 ; i<Total_Vertices*3; i = i + 3){
-        printf("%d%d%d ", Conjunto[i], Conjunto[i+1], Conjunto[i+2]);
-    }
-
-    return 1;
 }
 
-
 void Kruskal(Aresta* Grafo, Aresta* MST, int *Conjunto, int Total_Arestas, int Total_Vertices){
-    int i, Arestas_Colocadas = 0;
+    int i, Arestas_Colocadas = 0, aux;
 
     for(i=0; i< Total_Arestas; i++){
         if( Verifica_Conjunto( (Grafo + i)->origem, (Grafo + i)->Destino, Conjunto, Total_Vertices) != 10 ){
-
             // só vai cair aqui caso o valor do retorno for 1, significando que estão em diferentes conjuntos
 
+            // aux vai receber o valor do conjunto do destino para juntar todos depois
+            aux = Conjunto[(Grafo + i)->Destino];
+
+            for(int j=0; j< Total_Arestas; j++){
+                // percorre o vetor de conjunto para ver quais valores estão no conjunto do destino, assim ele converte todos
+                // os que pertencem ao da origem e destino em 1 só conjunto
+                if(Conjunto[j] == aux){
+                    Conjunto[j] = Conjunto[(Grafo + i)->origem]; 
+                }
+            }
+
+            /* usei para printar o vetor dos conjuntos, para verificar se estavam se juntando
+                printf("\n No meio:\n");
+                for(int i=0 ; i<Total_Vertices; i++){
+                    printf("%d ", Conjunto[i]);
+                }
+            */
+
+            // atribui a aresta atual para uma var auxiliar que vai receber os que pertencem ao MST 
             (MST + Arestas_Colocadas)->origem = (Grafo + i)->origem;
             (MST + Arestas_Colocadas)->valor = (Grafo + i)->valor;
             (MST + Arestas_Colocadas)->Destino = (Grafo + i)->Destino;
 
+            // incrementa para não substituir o valor no próximo laço
             Arestas_Colocadas++;
         }
-    }
-}
-
-void ZeraAux(Aresta* MST, int Total_Vertices){
-    int i;
-    for(i=0; i<Total_Vertices; i++){
-        (MST + i)->origem = 0;
-        (MST + i)->Destino = 0;
-        (MST + i)->valor = 0;       
     }
 }
 
@@ -183,6 +134,9 @@ void Ordernar(Aresta* Grafo, int Total_Arestas){
     int i, j;
     Aresta *aux;
 
+    aux =  (Aresta*) malloc( (Total_Arestas)*sizeof(Aresta) );
+
+    // implementado um bubblesort
     for(i=1; i < Total_Arestas; i++){
         for(j=0; j < Total_Arestas - 1; j++){
             if( (Grafo + j)->valor > (Grafo + (j + 1) )->valor ){
@@ -201,7 +155,7 @@ void Ordernar(Aresta* Grafo, int Total_Arestas){
             }
         }
     }
-
+    free(aux);
 }
 
 Aresta* Criar(int Total_arestas){
