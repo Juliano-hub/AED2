e46c40fb4557e2c76e94926da4148fb63f860428
Juliano-hub <juliano.buss@gmail.com>
2021-05-02 13:21:29 -0300

    initial upload

--- Kruskal.c
+++ Kruskal.c
@@ -7,14 +7,14 @@ typedef struct{
     int Destino;
 }Aresta;
 
-
 Aresta* Criar(int Total_arestas);
 void PreencheStruct(Aresta* Grafo);
 void Ordernar(Aresta* Grafo, int Total_Arestas);
 void ZeraAux(Aresta* MST, int Total_Vertices);
 void Kruskal(Aresta* Grafo, Aresta* MST, int *Conjunto, int Total_Arestas, int Total_Vertices);
 void PreencheConjunto(int *Conjunto, int Total_Vertices);
-int Verifica_Conjunto(int origem, int destino, int *Conjunto);
+int Verifica_Conjunto(int origem, int destino, int *Conjunto, int Total_Vertices);
+void printar_resultado(Aresta *MST, int Total_Vertices);
 
 int main (){
     Aresta *Grafo, *MST;
@@ -28,8 +28,21 @@ int main (){
     Grafo = Criar(Total_Arestas);
     PreencheStruct(Grafo);
 
+    for(int i=0 ; i<Total_Arestas; i++){
+        printf("\nOrigem:%d ", (Grafo+i)->origem);
+        printf("Destino:%d ", (Grafo+i)->Destino);
+        printf("Valor:%d ", (Grafo+i)->valor);
+    }
+
     // aplica o algoritmo bubbleshort para fazer a ordenação das arestas pelo seu valor/peso
     Ordernar(Grafo, Total_Arestas);
+
+    printf("\n-----Apos ordernar:\n");
+    for(int i=0 ; i<Total_Arestas; i++){
+        printf("\nOrigem:%d ", (Grafo+i)->origem);
+        printf("Destino:%d ", (Grafo+i)->Destino);
+        printf("Valor:%d ", (Grafo+i)->valor);
+    }
 
     // cria um auxiliar para receber as arestas válidas
     MST = Criar( (Total_Vertices - 1) );
@@ -44,31 +57,43 @@ int main (){
     Kruskal(Grafo, MST, Conjunto, Total_Arestas, Total_Vertices);
 
 
-    printf("\n");
+    printf("\n No fim:\n");
     for(int i=0 ; i<Total_Vertices*3; i = i + 3){
         printf("%d%d%d ", Conjunto[i], Conjunto[i+1], Conjunto[i+2]);
     }
 
+    printar_resultado(MST, Total_Vertices);
+
     return 0;
+}
+
+void printar_resultado(Aresta *MST, int Total_Vertices){
+    int i;
+    printf("\na = 0, b = 1, c = 2, d = 3, e = 4, f = 5, g = 6, h = 7, i = 8");
+    printf("\n-----As arestas resultantes sao:\n");
+    for(i=0; i< Total_Vertices - 1; i++){
+        printf("\nOrigem:%d ", (MST+i)->origem);
+        printf("Destino:%d ", (MST+i)->Destino);
+        printf("Valor:%d ", (MST+i)->valor);        
+    }
+
 }
 
 void PreencheConjunto(int *Conjunto, int Total_Vertices){
     int i, j=0;
     for(i=0 ; i<Total_Vertices; i = i + 3, j++){
-        Conjunto[i] = 1;
+        Conjunto[i] = -1;
         Conjunto[i + 1] = j;
-        Conjunto[i + 2] = 1;
+        Conjunto[i + 2] = -1;
     }
 
-
+        printf("\n");
     for(i=0 ; i<Total_Vertices; i = i + 3){
         printf("%d%d%d ", Conjunto[i], Conjunto[i+1], Conjunto[i+2]);
     }
-
-
 }
 
-int Verifica_Conjunto(int origem, int destino, int *Conjunto){
+int Verifica_Conjunto(int origem, int destino, int *Conjunto, int Total_Vertices){
     int i=0, j;
 
     while(Conjunto[i] != origem){
@@ -78,23 +103,21 @@ int Verifica_Conjunto(int origem, int destino, int *Conjunto){
 
     j = i;
 
-    if(Conjunto[i - 1] != 1){
-    //se estiver em uma posição no meio do conjunto
+    if(Conjunto[i - 1] != -1){
+    //se estiver em uma posição que não é a primeira do conjunto
 
-        while(Conjunto[i] != 1){
-
+        while(Conjunto[i] != -1){
             //----------eu vou voltar até achar o inicio desse conjunto
-
             if(Conjunto[i] == destino){
                 // se o valor do destino estiver em uma posição atrás do inicio nesse 
                 //conjunto, eu posso sair dessa função, já q eles estão juntos
                 return 10;
             }
-        i--;    
+        i = i - 1;    
         }
     }
 
-    while(Conjunto[j] != 1){
+    while(Conjunto[j] != -1){
     //----------se eu procurei pelas posições atrás e não encontrei o destino, vou procurar até eu achar o outro 1,
     //significando que o destino pode estar a uma posição a frente do inicio
 
@@ -107,16 +130,24 @@ int Verifica_Conjunto(int origem, int destino, int *Conjunto){
     }
 
     //----------só vai cair aqui caso eles não estiverem no mesmo conjunto
-    // procuro pela posição do destino
-
     i=0;
     while(Conjunto[i] != destino){
-            i++;
+        // procuro pela posição do destino
+        i++;
     }
 
     // como antes eu já salvei em j a posição do extremo 1, já recupero ele para fazer a junção dos conjuntos
-    Conjunto[j] = destino;
-    Conjunto[i] = 1;
+    while(Conjunto[i] != -1){
+            Conjunto[j] = Conjunto[i];
+            Conjunto[i] = -1;
+        i++;
+        j++;
+    }
+
+    printf("\n No meio:\n");
+    for(int i=0 ; i<Total_Vertices*3; i = i + 3){
+        printf("%d%d%d ", Conjunto[i], Conjunto[i+1], Conjunto[i+2]);
+    }
 
     return 1;
 }
@@ -126,7 +157,7 @@ void Kruskal(Aresta* Grafo, Aresta* MST, int *Conjunto, int Total_Arestas, int T
     int i, Arestas_Colocadas = 0;
 
     for(i=0; i< Total_Arestas; i++){
-        if( Verifica_Conjunto( (Grafo + i)->origem, (Grafo + i)->Destino, Conjunto) != 10 ){
+        if( Verifica_Conjunto( (Grafo + i)->origem, (Grafo + i)->Destino, Conjunto, Total_Vertices) != 10 ){
 
             // só vai cair aqui caso o valor do retorno for 1, significando que estão em diferentes conjuntos
 
@@ -156,14 +187,16 @@ void Ordernar(Aresta* Grafo, int Total_Arestas){
         for(j=0; j < Total_Arestas - 1; j++){
             if( (Grafo + j)->valor > (Grafo + (j + 1) )->valor ){
 
-                aux = (Grafo + j);
+                aux->Destino = (Grafo + j)->Destino;
+                aux->origem = (Grafo + j)->origem;
+                aux->valor = (Grafo + j)->valor;
 
                 (Grafo + j)->Destino = (Grafo + (j + 1) )->Destino;
                 (Grafo + j)->origem = (Grafo + (j + 1) )->origem;
                 (Grafo + j)->valor = (Grafo + (j + 1) )->valor;
 
                 (Grafo + (j + 1) )->Destino = aux->Destino;
-                (Grafo + (j + 1) )->origem = aux->valor;
+                (Grafo + (j + 1) )->origem = aux->origem;
                 (Grafo + (j + 1) )->valor = aux->valor;
             }
         }
